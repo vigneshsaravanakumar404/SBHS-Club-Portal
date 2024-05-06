414a8fb7b547c32e9c3225e9a62c30827bf2df04
Krish Renjen <krish.renjen@gmail.com>
2024-05-06 13:52:18 -0400

    added vignesh's dashboard

--- website/app/dashboard/page.tsx
+++ website/app/dashboard/page.tsx
@@ -1,110 +1,39 @@
-import CreateEventMenu from "@/components/CreateEvent/CreateEvent";
-import prisma from '@/lib/db'
-import { getServerSession } from "next-auth/next";
+import * as React from "react";
+import Header from "@/components/Dashboard/Header";
+import Footer from "@/components/Dashboard/Footer";
 
-// Constants
-const iconUrl = {
-  icon: "https://cdnsm5-ss18.sharpschool.com/UserFiles/Servers/Server_199481/Image/sbhs.png",
-  alt: "Viking Logo",
-  link: "#",
-};
-const Pages = [
-  { name: "Your Clubs", url: "#" },
-  { name: "Join Club", url: "#" },
-  { name: "Calendar", url: "#" },
-];
-const buttons = [{ name: "Log Out", url: "#" }];
+// const acceptedIPs = "50.206.77.";
 
-function header() {
-  return (
-    <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href={iconUrl.link} className="flex items-center">
-            <img src={iconUrl.icon} className="mr-3 h-6 sm:h-9" alt={iconUrl.alt} />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Clubs</span>
-          </a>
-          <div className="flex items-center lg:order-2">
-            {buttons.map((button) => (
-              <a
-                key={button.name}
-                href={button.url}
-                className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-              >
-                {button.name}
-              </a>
-            ))}
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              {Pages.map((page) => (
-                <li key={page.name}>
-                  <a
-                    href={page.url}
-                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    {page.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
-}
+// export function Body() {
+//   const [ip, setIp] = useState<string>("");
+//   useEffect(() => {
+//     fetch("https://api.ipify.org?format=json")
+//       .then((response) => response.json())
+//       .then((data) => setIp(data.ip));
+//   }, []);
+//   const trigger = !ip.startsWith(acceptedIPs);
+//   const { data: session } = useSession();
 
-function Content() {
+//   if (session) {
+//     const { user } = session;
+
+//     return (
+//       <div className="p-[20px]">
+//         <p>Your ip is {ip}</p>
+//         {trigger ? <p>Unauthorized</p> : null}
+//         {!trigger ? <p>Authorized</p> : null}
+//       </div>
+//     );
+//   }
+//   return null;
+// }
+
+export default function Page() {
   return (
     <div>
-      <p>Layout for Clubs</p>
-    </div>
-  );
-}
-
-export default async function Page() {
-  const session = await getServerSession();
-  var user = null;
-  if(session?.user?.email){
-    user = await prisma.user.findFirst({ where: { email: session.user.email }, include: { leadershipFor: true, advisorFor: true} })
-  }
-
-  var allowedToCreateEvent = false;
-  if (user) {
-    allowedToCreateEvent = ['ADVISOR', 'TEACHER', 'ADMIN'].includes(user.role) || 
-                           (user.role === 'DEFAULT' && (user.leadershipFor.length > 0 || user.advisorFor.length > 0));
-  }
-  return (
-    <div>
-      {header()}
-      <br />
-      {Content()}
-      {allowedToCreateEvent && user && <CreateEventMenu associations={user.leadershipFor.concat(user.advisorFor)}/>}
-      {user ? JSON.stringify(await user) : "how did you get here"}
+      <Header />
+      {/* <Body /> */}
+      <Footer />
     </div>
   );
 }
