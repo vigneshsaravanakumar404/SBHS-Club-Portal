da5e423829fdacdb44127edd2a3d7a3864cb3136
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-05-06 12:26:28 -0400

    Second Commit
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- website/app/Header.tsx
+++ website/app/Header.tsx
@@ -1,6 +1,5 @@
-"use client";
+"use server"
 import { Avatar, AvatarFallback } from "@/components/ui/avatar"
-import { useSession } from 'next-auth/react';
 import { Button } from "@/components/ui/button"
 import {
     DropdownMenu,
@@ -9,6 +8,9 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { getServerSession } from "next-auth";
+import prisma from "@/lib/db";
+import CreateEventMenu from "@/components/CreateEvent/CreateEvent";
 require("./style.css");
 
 
@@ -19,56 +21,74 @@ const links = [
     { href: '/dashboard', label: 'Join' }
 ];
 
-export default function Header() {
+export default async function Header() {
 
-    const { data: session } = useSession();
+    const session = await getServerSession();
+    var user = null;
+    if (session?.user?.email) {
+        user = await prisma.user.findFirst({ where: { email: session.user.email }, include: { leadershipFor: true, advisorFor: true } })
+    }
 
-    if (session) {
+    var allowedToCreateEvent = false;
+    if (user) {
+        allowedToCreateEvent = ['ADVISOR', 'TEACHER', 'ADMIN'].includes(user.role) ||
+            (user.role === 'DEFAULT' && (user.leadershipFor.length > 0 || user.advisorFor.length > 0));
 
-        const { user } = session;
-        const name = user?.name ?? 'Anonymous';
-        const image = user?.image ?? 'https://cdn-icons-png.flaticon.com/512/20/20079.png';
+        console.log(allowedToCreateEvent)
 
-        return (
-            <nav className="navbar">
-                <div className="nav-header">
-                    <div className="logo">
-                        <h1>LOGO</h1>
+
+
+        if (session) {
+
+            const { user } = session;
+            const name = user?.name ?? 'Anonymous';
+            const image = user?.image ?? 'https://cdn-icons-png.flaticon.com/512/20/20079.png';
+
+            return (
+                <nav className="navbar">
+                    <div className="nav-header">
+                        <div className="logo">
+                            <h1>LOGO</h1>
+                        </div>
+                        <button className="toggle-button">&#9776;</button> {/* Hamburger icon */}
                     </div>
-                    <button className="toggle-button">&#9776;</button> {/* Hamburger icon */}
+                    <ul className="nav-links">
+                        {links.map(({ href, label }) => (
+                            <li key={`${href}${label}`}>
+                                <a href={href}>{label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="flex flex-row items-center justify-center gap-2">
+                        {/* {allowedToCreateEvent && user && <CreateEventMenu associations={(user).leadershipFor.concat((user as any).advisorFor)}/>}                     */}
+                        <div className="avatar">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Avatar>
+                                    <img src={image} alt="Avatar" referrerPolicy="no-referrer" />
+                                    <AvatarFallback>{name?.charAt(0)}</AvatarFallback>
+                                </Avatar>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-16">
+                                <DropdownMenuLabel style={{ textAlign: "center" }}>{name}</DropdownMenuLabel>
+                                {/* <DropdownMenuSeparator /> */}
+                                <div className="button-container">
+                                    <Button variant="secondary" asChild style={{ width: "100%" }}>
+                                        <a href="/settings">Settings</a>
+                                    </Button>
+                                    <Button variant="secondary" asChild style={{ width: "100%" }}>
+                                        <a href="/api/auth/signout">Logout</a>
+                                    </Button>
+                                </div>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
                 </div>
-                <ul className="nav-links">
-                    {links.map(({ href, label }) => (
-                        <li key={`${href}${label}`}>
-                            <a href={href}>{label}</a>
-                        </li>
-                    ))}
-                </ul>
-                <div className="avatar">
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Avatar>
-                                <img src={image} alt="Avatar" referrerPolicy="no-referrer" />
-                                <AvatarFallback>{name?.charAt(0)}</AvatarFallback>
-                            </Avatar>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="w-16">
-                            <DropdownMenuLabel style={{ textAlign: "center" }}>{name}</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <div className="button-container">
-                                <Button variant="secondary" asChild style={{ width: "100%" }}>
-                                    <a href="/settings">Settings</a>
-                                </Button>
-                                <Button variant="secondary" asChild style={{ width: "100%" }}>
-                                    <a href="/logout">Logout</a>
-                                </Button>
-                            </div>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
-            </nav>
+
+            </nav >
         );
 
+        }
+        return null;
     }
-    return null;
 }
