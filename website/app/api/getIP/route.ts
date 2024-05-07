71a9f5c6586a4900b45b03843b3e1b32f6f45dca
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-05-07 10:08:31 -0400

    Format Files
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- website/app/api/getIP/route.ts
+++ website/app/api/getIP/route.ts
@@ -2,17 +2,17 @@ import { NextRequest, NextResponse, userAgent } from "next/server";
 import { headers } from "next/headers";
 
 /** @param {NextRequest} req */
-export async function GET(req: { headers: Headers; }) {
-  const { ua } = userAgent(req);
+export async function GET(req: { headers: Headers }) {
+    const { ua } = userAgent(req);
 
-  const headersList = headers();
-  const ip = headersList.get("x-forwarded-for");
+    const headersList = headers();
+    const ip = headersList.get("x-forwarded-for");
 
-  const data = {
-    ok: true,
-    ip_address: ip,
-    user_agent: ua,
-  };
+    const data = {
+        ok: true,
+        ip_address: ip,
+        user_agent: ua,
+    };
 
-  return NextResponse.json(data, { status: 200 });
+    return NextResponse.json(data, { status: 200 });
 }
