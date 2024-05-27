f09c9f7608bbc40f1c8ccb2693f5b32e560fd519
Krish Renjen <krish.renjen@gmail.com>
2024-05-27 18:25:42 -0400

    geo and ip

--- website/app/location/page.tsx
+++ website/app/location/page.tsx
@@ -1,15 +1,15 @@
-"use client"
-import { useEffect, useState } from 'react';
+"use client";
+import { stringify } from "querystring";
+import { useEffect, useState } from "react";
 
-const latitudeSB = 40.373631
-const longitudeSB = -74.563698
+const latitudeSB = 40.373631;
+const longitudeSB = -74.563698;
 
 export default function Location() {
-
-  const [location, setLocation] = useState<{ latitude: number, longitude: number }>({ latitude: 0, longitude: 0 });
+  const [location, setLocation] = useState<{ latitude: number; longitude: number }>({ latitude: 0, longitude: 0 });
 
   useEffect(() => {
-    if ('geolocation' in navigator) {
+    if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(({ coords }) => {
         const { latitude, longitude } = coords;
         setLocation({ latitude, longitude });
@@ -17,23 +17,24 @@ export default function Location() {
     }
   }, []);
 
-  return <div>{getDistanceFromLatLonInKm(location.latitude, location.longitude, latitudeSB, longitudeSB)}</div>
+  return (
+    <div>
+      <div>{getDistanceFromLatLonInKm(location.latitude, location.longitude, latitudeSB, longitudeSB)}</div>
+      <div>{JSON.stringify(location)}</div>
+    </div>
+  );
 }
-
 
 function getDistanceFromLatLonInKm(lat1: number, lon1: number, lat2: number, lon2: number) {
   var R = 6371; // Radius of the earth in km
   var dLat = deg2rad(lat2 - lat1);
   var dLon = deg2rad(lon2 - lon1);
-  var a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   var d = R * c; // Distance in km
   return d;
 }
 
 function deg2rad(deg: number) {
-  return deg * (Math.PI / 180)
+  return deg * (Math.PI / 180);
 }
